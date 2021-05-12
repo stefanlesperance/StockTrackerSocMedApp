ac15b71787da15c91c41456522112bfe003787b8
Stefan L'Esperance <slesperance@conamex.com>
2021-05-12 07:13:21 -0400

    JS changes (commented out code continued to be read), flash message adjustments)

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,23 +1,23 @@
 class StocksController < ApplicationController
-  
-	def search
-		#Check for the presence of params. His idea, not mine, should have thought of it.
-		#Best option - it ensures we do not make a useless API call.
-		if params[:stock].present?
-			#if @stock is effectively a method of checking it exists - if it is not nil, then it would default elsewhere.
-			#Ironically, it worked without the return 
-			@stock = Stock.new_lookup(params[:stock])
-			if @stock
-				respond_to do |format|
-					format.js {render partial: 'users/result'}
-				end
-			else
-				flash[:alert] = "That stock does not exist"
-				render 'users/my_portfolio'
-			end
-		else
-			flash[:alert] = "Please enter a Stock symbol"
-			render 'users/my_portfolio'			
-		end
-	end
+
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol to search"
+          format.js { render partial: 'users/result' }
+        end
+      end    
+    else
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
+    end
+  end
 end
