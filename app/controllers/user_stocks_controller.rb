eed727f535f59e2786cc6448a194d3d7ef00db19
Stefan L'Esperance <slesperance@conamex.com>
2021-05-14 06:28:57 -0400

    My Version of Stopping them at 10.

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -6,18 +6,26 @@ class UserStocksController < ApplicationController
 		#We can use this stock = Stock.new_lookup(params[:ticker])
 		#But notice that new_lookup really works best if the stock is not already present - what if it IS present? 
 		
-		#Database check method. See if the stock is in the Database
-		stock = Stock.check_db(params[:ticker])
-			#If it isn't we run the If Statement
-		if stock.blank?
-			#This looks up the Stock via IEX API
-			stock = Stock.new_lookup(params[:ticker])
-			#Then we save the stock to the database
-			stock.save
-			#Now we can pass both the User id, and the stock id, straight to the UserStocks table, creating the relationship
-			@user_stock = UserStock.create(user: current_user, stock: stock)
-			flash[:notice] = "Stock #{stock.name}was successfully added to your portfolio."
+		#New addition - Maximum ten tracked stocks, I can simple kill it off from here. If I can code it properly
+
+		if current_user.stocks.count >= 10
+			flash[:alert] = "You have reached the maximum number of allowable tracked stocks."
 			redirect_to my_portfolio_path
+		else
+
+			#Database check method. See if the stock is in the Database
+			stock = Stock.check_db(params[:ticker])
+				#If it isn't we run the If Statement
+			if stock.blank?
+				#This looks up the Stock via IEX API
+				stock = Stock.new_lookup(params[:ticker])
+				#Then we save the stock to the database
+				stock.save
+				#Now we can pass both the User id, and the stock id, straight to the UserStocks table, creating the relationship
+				@user_stock = UserStock.create(user: current_user, stock: stock)
+				flash[:notice] = "Stock #{stock.name}was successfully added to your portfolio."
+				redirect_to my_portfolio_path
+			end
 		end
 	end
 
