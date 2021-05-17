5a6c3977675cea0b5c471888c348a610d6c0ac06
Stefan L'Esperance <slesperance@conamex.com>
2021-05-17 05:40:49 -0400

    Friends List - Tried to do Self-Delete, isn't currently working.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
   def friends
-  	@user = current_user
 	@friends_list = current_user.friends
   end
 
   def my_portfolio
   	@tracked_stocks = current_user.stocks
+  end
+
+  def destroy_friend_link
+  	byebug
+  	friend = Friendship.where(user_id: current_user.id, friend: friend.id).first
+  	friend.destroy
+  	flash[:notice] = "#{friend.full_name} was successfully removed from portfolio"
+	redirect_to users_friends_path
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+	User.create(email: "Test8@Test.com", first_name: "Jolie", last_name: "Jacques-Rousseau", password: "password")
+	User.create(email: "Test9@Test.com", first_name: "Juliette", last_name: "Jacques-Rousseau", password: "password")
+	User.create(email: "Test10@Test.com", first_name: "Julie", last_name: "Jacques-Rousseau", password: "password")
+	User.create(email: "Test11@Test.com", first_name: "Jeanette", last_name: "Jacques-Rousseau", password: "password")
+	User.create(email: "Test12@Test.com", first_name: "JeNeSaisQuoi", last_name: "Jacques-Rousseau", password: "password")
