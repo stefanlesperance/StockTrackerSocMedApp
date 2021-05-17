a1e5582a461444f77b1f2c712022786db1442345
Stefan L'Esperance <slesperance@conamex.com>
2021-05-17 04:42:09 -0400

    Personal Version Friends List Page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   get 'my_portfolio', to: 'users#my_portfolio'
+  get 'users/friends'
   #This lets me create any route, period, it doesn't have to be a page. Remember that. I created this specificallyt to route to-
   # the one controller action.
   get 'search_stock', to: 'stocks#search'
